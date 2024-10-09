5b8b32b733a7379cee3fc03c2e1a55d1df57a861
Kanae <kanae.bunche@gmail.com>
2024-10-09 12:37:50 -0400

    build

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -27,7 +27,14 @@ const Projects = () => {
                 </p>
                 <a href="https://www.cybermissions.org/" className="project-link">View Project</a>
             </div>
-
+            <div className="project" data-aos="fade-up">
+                <img src={casesImage} alt="Shopping Experience" className="project-image" />
+                <h3 className="project-name">Cases</h3>
+                <p className="project-description">
+                    This vibrant React.js website delivers an engaging shopping experience, featuring intuitive add-to-cart and checkout options, along with a wishlist for favorites, all designed for seamless navigation and user satisfaction.
+                </p>
+                <a href="https://my-6ij1tn11j-kanaebunches-projects.vercel.app/" className="project-link">View Project</a>
+            </div>
             <div className="project" data-aos="fade-up">
                 <img src={bloggersImage} alt="Blogging Platform" className="project-image" />
                 <h3 className="project-name">Blogging Platform: Bloggers</h3>
@@ -35,15 +42,6 @@ const Projects = () => {
                     I created a responsive blogging platform where users can write and publish articles. The app features a rich text editor powered by React Draft WYSIWYG, allowing users to easily format and customize their content. Users can preview their work before publishing, and stories are saved locally for quick access. With a clean design, smooth navigation, and an intuitive interface, this app provides a seamless writing experience for bloggers.
                 </p>
                 <a href="https://www.loom.com/share/65e3d15e598c40ae9d5c48e3a476ddc3?sid=297480eb-791a-4423-b03a-e874acbaa7a3" className="project-link">View Project</a>
-            </div>
-
-            <div className="project" data-aos="fade-up">
-                <img src={casesImage} alt="Shopping Experience" className="project-image" />
-                <h3 className="project-name">Cases</h3>
-                <p className="project-description">
-                    This vibrant React.js website delivers an engaging shopping experience, featuring intuitive add-to-cart and checkout options, along with a wishlist for favorites, all designed for seamless navigation and user satisfaction.
-                </p>
-                <a href="https://my-app-delta-coral.vercel.app/" className="project-link">View Project</a>
             </div>
 
             {/* Contact section */}
